c14819a00fbc46552e8696be27273928869a4858
Kinga <kingaewa05@gmail.com>
2023-02-01 20:15:17 +0100

    Pierwsze wprowadzone zmiany

--- src/App.js
+++ src/App.js
@@ -6,13 +6,30 @@ import './App.css';
 function App() {
   const [actNumber, setActNumber] = useState(0);
   const [prevNumber, setPrevNumber] = useState(0);
+  const [operand, setOperand] = useState("");
 
-  const operator =()=>{
+  const operator =(action)=>{
     setPrevNumber(actNumber);
     setActNumber(0);
 
-    
-
+    switch(action){
+      case 1:
+        setOperand('÷');
+        break;
+      case 2:
+        setOperand('×');
+        break;
+      case 3:
+        setOperand('-');
+        break;
+      case 4:
+        setOperand('+');
+  }
+  }
+  const deleting = ()=>{
+    setActNumber(0);
+    setPrevNumber(0);
+    setOperand("");
   }
 
   return (
@@ -21,27 +38,27 @@ function App() {
       <div className="screen">
         <div className="smallNumbers">
           <div className="prevNumber">{prevNumber}</div>
-          <div className="operation">+</div>
+          <div className="operation">{operand}</div>
         </div>
         <div className="actNumber">{actNumber}</div>
       </div>
 
       <div className="buttons">
-        <button className="twoplace" onClick={() => setActNumber(0)}>AC</button>
+        <button className="twoplace" onClick={deleting}>AC</button>
         <button className="oneplace" >DEL</button>
-        <button className="oneplace" onClick={operator}>/</button>
+        <button className="oneplace" onClick={() => operator(1)}>÷</button>
         <button className="oneplace" onClick={() => setActNumber(actNumber*10 + 7)}>7</button>
         <button className="oneplace" onClick={() => setActNumber(actNumber*10 + 8)}>8</button>
         <button className="oneplace" onClick={() => setActNumber(actNumber*10 + 9)}>9</button>
-        <button className="oneplace" onClick={operator}>*</button>
+        <button className="oneplace" onClick={() => operator(2)}>×</button>
         <button className="oneplace" onClick={() => setActNumber(actNumber*10 + 4)}>4</button>
         <button className="oneplace" onClick={() => setActNumber(actNumber*10 + 5)}>5</button>
         <button className="oneplace" onClick={() => setActNumber(actNumber*10 + 6)}>6</button>
-        <button className="oneplace" onClick={operator}>-</button>
+        <button className="oneplace" onClick={() => operator(3)}>-</button>
         <button className="oneplace" onClick={() => setActNumber(actNumber*10 + 1)}>1</button>
         <button className="oneplace" onClick={() => setActNumber(actNumber*10 + 2)}>2</button>
         <button className="oneplace" onClick={() => setActNumber(actNumber*10 + 3)}>3</button>
-        <button className="oneplace" onClick={operator}>+</button>
+        <button className="oneplace" onClick={() => operator(4)}>+</button>
         <button className="oneplace" >.</button>
         <button className="oneplace" onClick={() => setActNumber(actNumber*10 + 0)}>0</button>
         <button className="twoplace" onClick={operator}>=</button>
